d49bd03db31b2eec4cec28e174313d82c3d0b875
Gena Abakumov <abakumov.gena@gmail.com>
2025-07-26 23:02:20 +0300

    llama 3.1

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from langchain_ollama.llms import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
 from vector import retriever
 
-model = OllamaLLM(model="llama3.2")
+model = OllamaLLM(model="llama3.1")
 
 template = """
 You are an exeprt in answering questions about a pizza restaurant
@@ -15,12 +15,12 @@ prompt = ChatPromptTemplate.from_template(template)
 chain = prompt | model
 
 while True:
-    print("\n\n-------------------------------")
+    print("\n-------------------------------")
     question = input("Ask your question (q to quit): ")
-    print("\n\n")
+    print("\n")
     if question == "q":
         break
-    
+
     reviews = retriever.invoke(question)
     result = chain.invoke({"reviews": reviews, "question": question})
     print(result)
